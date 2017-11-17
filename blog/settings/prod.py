910414b4759e48750045e7bba6f08e1680d54989
Elias S <e.j.scarborough@gmail.com>
2017-11-16 21:36:18 -0600

    updated settings

--- blog/settings/prod.py
+++ blog/settings/prod.py
@@ -83,7 +83,7 @@ EMAIL_USE_TLS = True
 
 DATABASES = {}
 
-db_from_env = dj_database_url.config(conn_max_age=500)
+db_from_env = dj_database_url.config(default=os.environ['DATABASE_URL'], conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
 # Password validation
