de357fe2ad7338b6cda09987433128365b1f4cf5
Elias S <e.j.scarborough@gmail.com>
2017-11-18 14:42:02 -0600

    turning off debug

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from . import views
 
 urlpatterns = [
@@ -20,3 +20,4 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+urlpatterns += staticfiles_urlpatterns()
