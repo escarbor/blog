4cc39e3d626906b495d3b348475237a86c0b0f9b
Elias S <e.j.scarborough@gmail.com>
2017-11-18 09:05:40 -0600

    turning off debug

--- blog/settings/prod.py
+++ blog/settings/prod.py
@@ -130,12 +130,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-
-STATIC_ROOT = os.path.join(PROJECT_ROOT, '../staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, '../staticfiles')
 
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, '../static'),
+    os.path.join(BASE_DIR, '../static'),
 )
 
 if DEBUG:
