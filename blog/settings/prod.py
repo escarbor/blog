0e62737406490d65bcd31cd280d8bf60bcca80da
Elias S <e.j.scarborough@gmail.com>
2017-11-18 09:13:42 -0600

    turning off debug

--- blog/settings/prod.py
+++ blog/settings/prod.py
@@ -128,9 +128,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, '../staticfiles')
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, '../static'),
