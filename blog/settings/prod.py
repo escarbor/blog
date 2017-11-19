225f6905a0869072adc2180716239cd79ff3f2cd
Elias S <e.j.scarborough@gmail.com>
2017-11-19 15:39:56 -0600

    changed settings and added custom error views

--- blog/settings/prod.py
+++ blog/settings/prod.py
@@ -142,13 +142,13 @@ USE_TZ = True
 
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_ROOT = [os.path.join(os.path.join(BASE_DIR, '../staticfiles'))]
+STATIC_ROOT = os.path.join(os.path.join(BASE_DIR, '../staticfiles'))
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = [
+STATICFILES_DIRS = (
     os.path.join(BASE_DIR, '../static'),
-]
+)
 
 
 # Simplified static file serving.
