fef976a4ddbb916044f4ee2466add8e34d177e60
Elias S <e.j.scarborough@gmail.com>
2017-11-19 14:44:09 -0600

    changed settings and added custom error views

--- blog/settings/prod.py
+++ blog/settings/prod.py
@@ -127,8 +127,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_URL = '/staticfiles/'
+STATIC_URL = '/static/'
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 STATIC_ROOT = os.path.join(BASE_DIR, '../staticfiles')
 
