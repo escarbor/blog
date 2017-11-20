3fbf22ad69c971b874c860c7e003699f27b22f80
Elias S <e.j.scarborough@gmail.com>
2017-11-19 20:42:50 -0600

    changed settings and added custom error views

--- blog/settings/prod.py
+++ blog/settings/prod.py
@@ -126,8 +126,9 @@ USE_TZ = True
 
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/staticfiles/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
